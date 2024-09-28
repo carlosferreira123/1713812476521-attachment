720941dec22008cfc352ff830521e50a9a24d426
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-28 20:09:01 -0300

    Adicionando conteudo

--- template/scripts.js
+++ template/scripts.js
@@ -1,24 +1,9 @@
-// Visualizar o conteudo do document.
-// conole.log(document)
+const guest = document.querySelector("#guest-1 ")
 
-// Obter o title da pagina.
-console.log(document.title)
+// Retorna o conteudo  como texto.
+// console.log(guest.textContent)
+// guest.textContent = "Maria"
 
-// Acessar o elemenrto pelo ID
-const guest = document.getElementById("guest-2")
-console.log(guest)
-
-// Mostrar o elemento
-console.dir(guest)
-
-// Acessar elemento com class (SELETOR class)
-const guestsByClass = document.getElementsByClassName("guest")
-console.log(guestsByClass)
-
-// Exibir o primeiro elemento da lista. 
-console.log(guestsByClass.item(0))
-console.log(guestsByClass[1])
-
-// Selecioanr lista de elementos pela tag
-const guestsTag = document.getElementsByTagName("li")
-console.log(guestsTag)
+console.log(guest.textContent) // Retorna o conteudo visivel e oculto
+console.log(guest.innerText) // Retoena somente o conteudo visivel
+console.log(guest.innerHTML) // Retorna o HTML como texto
--- template/scripts02.js
+++
@@ -1,5 +0,0 @@
-//Acessa o elemento pelo setector ID
-const guest = document.querySelector("#guest-1")
-
-// Acessa o elemento pela a classe
-const guests = document.querySelectorAll(".guest")
